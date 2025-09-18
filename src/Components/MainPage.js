ab53bcd542e9193ce1588b96a822038083296ef3
Pallavi-8555 <pallavikute35@gmail.com>
2025-09-18 19:28:59 +0530

    added useEffect hook

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,42 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import MealCards from './MealCards';
 
 const MainPage = () => {
-    const [data,setData]  =useState();
-    const [search, setSearch] = useState("");
-    const [msg, setMsg] = useState("")
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [msg, setMsg] = useState("");
 
-    const handleInput = (event) =>{
-        setSearch(event.target.value)
+  useEffect(() => {
+    const fetchInitialMeals = async () => {
+      try {
+        const res = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=");
+        const json = await res.json();
+        setData(json.meals);
+      } catch (err) {
+        setMsg("Failed to load meals");
+      }
+    };
+    fetchInitialMeals();
+  }, []);
+
+  const handleInput = (event) => setSearch(event.target.value);
+
+  const myFun = async () => {
+    if (search.trim() === "") {
+      setMsg("Please Enter Something");
+    } else {
+      const get = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+      );
+      const jsonData = await get.json();
+      setData(jsonData.meals);
+      setMsg("");
     }
-    const myFun = async () =>{
-        if(search == ""){
-            setMsg("Please Enter Something")
-        }else{
-            const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
-            const jsonData = await get.json();
-        setData(jsonData.meals)
-        setMsg("")
-        
-        }
-         
-    }
-    
+  };
+
   return (
     <>
-        <h1 className='head'>FOOD RECIPE APP</h1>
-        <div className='container'>
-            <div className='searchBar'>
-                <input type='text' placeholder='Enter Dishe' onChange={handleInput}/>
-                <button onClick={myFun}>Search</button>
-            </div>
-            <h4 className='msg'>{msg}</h4>
-            <div>
-            <MealCards detail={data}/>
-            </div>
+      <h1 className="head">FOOD RECIPE APP</h1>
+      <div className="container">
+        <div className="searchBar">
+          <input
+            type="text"
+            placeholder="Enter Dish"
+            onChange={handleInput}
+          />
+          <button onClick={myFun}>Search</button>
         </div>
+        <h4 className="msg">{msg}</h4>
+        <MealCards detail={data} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
